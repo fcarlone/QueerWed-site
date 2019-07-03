05c19af3c81d0e35155bd4a32415e6d7991e2c66
fcarlone <frankcarlone@gmail.com>
2019-07-03 17:13:21 -0400

    Setup server

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// Connect database
+
+// Middleware
+
+// Serve static assets
+
+// Define routes
+
+// Route to load single HTML page
+
+// Add listner
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
