09b85955f8d08ca86a9daff817688610959ed3a0
hyelim3316@gmail.com <hyelim3316@gmail.com>
2019-07-17 15:24:56 -0400

    website updated

--- client/src/components/pages/websitecomplete/Rsvp.js
+++ client/src/components/pages/websitecomplete/Rsvp.js
@@ -1,24 +1,59 @@
 import React, { useContext, useEffect } from "react";
 import GuestContext from "../../../context/guest/guestContext";
-import "../../../style/website/guestBook.css";
+import "../../../style/website/rsvp.css";
+import { PromiseProvider } from "mongoose";
 
 
-function Rsvp() {
+function Rsvp(props) {
 
   const guestContext = useContext(GuestContext);
   // const { deleteGuest, setCurrent, clearCurrent } = guestContext;
   const { guests, getGuests } = guestContext;
   
-
+  console.log("guest list", guests)
   useEffect(() => {
+    console.log("this is working")
     getGuests();
     // eslint-disable-next-line
   }, []);
 
   return(
-    <div className="jumbotron d-flex align-items-center" id="rsvp">
-    <div className="container vertical-center">
-      <h1>RSVP</h1>
+    <div className="jumbotron d-flex" id="rsvp">
+    <div className="container">
+      <h1>RSVP </h1>
+      <h3 id="secondline"> Please RSVP by {props.rsvpdate}</h3>
+    <div id="thirdline" className="text-center">
+      Your Name : <sapn>
+      <input id="findname"
+      onChange={props.handleRsvpName} 
+      value={props.value}
+      name= "guestname"
+      type="text"  
+      placeholder="Type your name" />
+    {/* <select id="findname" className="custom-select">
+      <option selected>Find Your name here</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>  */}
+    </sapn>
+    <div id="fourline" className="guestanswer">
+    <div className="form-check">
+    <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+  <label className="form-check-label" for="defaultCheck1">
+    Yes, I will be there
+  </label>
+  </div>
+  <div className="form-check">
+  <input className="form-check-input" type="checkbox" value="" id="defaultCheck2" />
+  <label className="form-check-label" for="defaultCheck2">
+    No, I won't be there
+  </label>
+  </div>
+    </div>
+    <button id="rsvpbutton" onClick={props.handleRsvpSubmitButton} type="button" 
+          className="btn btn-outline-secondary">Submit</button>
+    </div>
       <h3>
         {guests.map((guest) => (
           console.log(guest.name)
--- client/src/components/pages/websitecomplete/WebsiteNav.js
+++ client/src/components/pages/websitecomplete/WebsiteNav.js
@@ -4,16 +4,16 @@ import "../../../style/website/websiteNav.css";
 function Nav(props) {
   return(
   <ul className="nav justify-content-center row">
-    <li className="logo nav-item col-2 text-center">OUR WEDDING</li>
-    <li className="detail nav-item col-2">
+    <li className="logo nav-item col-5 text-left ml-2">OUR WEDDING</li>
+    <li className="detail nav-item col-2 text-left">
       <a className="nav-link js-scroll-trigger" href="#details">date &amp; location</a></li>
-    <li className="registry nav-item col-2">
-      <a className="nav-link js-scroll-trigger" href="#registry">registry</a></li>
-    <li className="guestbook nav-item col-2">
+    <li className="registry nav-item col-2 text-left">
+      <a className="nav-link js-scroll-trigge" href="#registry">registry</a></li>
+    {/* <li className="guestbook nav-item col-2">
       <a className="nav-link js-scroll-trigger" href="#guestbook">guest book</a></li>
     <li className="qa nav-item col-2">
-      <a className="nav-link js-scroll-trigger" href="#faqs">faqs</a></li>
-    <li className="rsvp nav-item col-2">
+      <a className="nav-link js-scroll-trigger" href="#faqs">faqs</a></li> */}
+    <li className="rsvp nav-item col-2 text-left">
       <a className="nav-link js-scroll-trigger" href="#rsvp">rsvp</a></li>
 
     {/* <li className="guess nav-item col-4">{props.message}</li>
